2de37c87dcef01428c5f019e074aa8d74e271862
Miri Kunisada <miriev56@gmail.com>
2017-09-17 15:12:43 +0900

    :sparkles: connecting react to redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar } from '../actions'
 
 class App extends Component {
+
   render() {
+    console.log(this.props)
     return (
       <div>
         Hello, World!
@@ -9,4 +13,32 @@ class App extends Component {
     )
   }
 }
-export default App
+
+function mapStateToProps(calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      //arry to obj
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+
+        return meals
+      }, {})
+    })),
+  }
+}
+
+
+//optional : you can just call dispatch method in the App component directly but it maybe more clear to use 
+function mapDispatchToProps(dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
